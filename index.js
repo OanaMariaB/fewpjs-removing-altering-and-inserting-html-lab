c027e81cb473b9d17b3bc129102722a724787de5
OanaMariaB <oana.berentan@yahoo.com>
2022-01-22 22:46:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove('main#main')
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "Oana is the champion";
+
